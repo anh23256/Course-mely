66c54b3d07124fbae600914826de27ca6fd9fce0
TungTvPh81102 <quaixe121811@gmail.com>
2025-02-22 07:39:31 +0700

    modify coding api

--- app/Services/CourseValidatorService.php
+++ app/Services/CourseValidatorService.php
@@ -3,7 +3,9 @@
 namespace App\Services;
 
 use App\Models\Chapter;
+use App\Models\Coding;
 use App\Models\Course;
+use App\Models\Document;
 use App\Models\Lesson;
 use App\Models\Question;
 use App\Models\Quiz;
@@ -44,9 +46,6 @@ class CourseValidatorService
         if (count($requirements) < 4 || count($requirements) > 10) {
             $errors[] = "Khóa học phải có từ 4 đến 10 yêu cầu.";
         }
-        if (count($qa) < 1 || count($qa) > 5) {
-            $errors[] = "Khóa học phải có từ 1 đến 5 câu hỏi thường gặp.";
-        }
 
         return $errors;
     }
@@ -64,6 +63,17 @@ class CourseValidatorService
             if (!$chapter->title) {
                 $errors[] = "Chương học ID {$chapter->id} không có tiêu đề.";
             }
+
+//            $totalVideoDuration = Video::whereIn('id', function ($query) use ($chapter) {
+//                $query->select('lessonable_id')
+//                    ->from('lessons')
+//                    ->where('chapter_id', $chapter->id)
+//                    ->where('lessonable_type', Video::class);
+//            })->sum('duration');
+//
+//            if ($totalVideoDuration < 1800) {
+//                $errors[] = "Tổng thời lượng video trong chương '{$chapter->title}' phải lớn hơn 30 phút. Hiện tại chỉ có " . round($totalVideoDuration / 60, 2) . " phút.";
+//            }
 
             $errors = array_merge($errors, self::validateLessons($chapter->id, $chapter->title));
         }
@@ -92,6 +102,14 @@ class CourseValidatorService
             if ($lesson->lessonable_type === Quiz::class) {
                 $errors = array_merge($errors, self::validateQuiz($lesson->lessonable_id, $lesson->title, $chapterTitle));
             }
+
+            if ($lesson->lessonable_type === Coding::class) {
+                $errors = array_merge($errors, self::validateCoding($lesson->lessonable_id, $lesson->title, $chapterTitle));
+            }
+
+            if ($lesson->lessonable_type === Document::class) {
+                $errors = array_merge($errors, self::validateDocument($lesson->lessonable_id, $lesson->title, $chapterTitle));
+            }
         }
 
         return $errors;
@@ -107,8 +125,8 @@ class CourseValidatorService
             return $errors;
         }
 
-        if ($video->duration < 900) {
-            $errors[] = "Bài giảng '{$lessonTitle}' trong chương '{$chapterTitle}' có video dưới 15 phút.";
+        if ($video->duration < 300) {
+            $errors[] = "Bài giảng '{$lessonTitle}' trong chương '{$chapterTitle}' có video dưới 5 phút.";
         }
 
         return $errors;
@@ -128,4 +146,37 @@ class CourseValidatorService
 
         return $errors;
     }
+
+    private static function validateCoding(int $codingId, string $lessonTitle, string $chapterTitle): array
+    {
+        $errors = [];
+        $coding = Coding::query()->find($codingId);
+
+        if ($coding) {
+            if (!$coding->title || !$coding->language) {
+                $errors[] = "Bài tập lập trình '{$lessonTitle}' trong chương '{$chapterTitle}' thiếu tiêu đề hoặc ngôn ngữ lập trình.";
+            }
+
+            if (!$coding->solution_code || !$coding->sample_code) {
+                $errors[] = "Bài tập lập trình '{$lessonTitle}' trong chương '{$chapterTitle}' thiếu mã lời giải hoặc mã mẫu.";
+            }
+        }
+
+        return [];
+    }
+
+    private static function validateDocument(int $documentId, string $lessonTitle, string $chapterTitle): array
+    {
+        $errors = [];
+        $document = Document::query()->find($documentId);
+
+        if ($document) {
+            if (!$document->title || !$document->content) {
+                $errors[] = "Tài liệu '{$lessonTitle}' trong chương '{$chapterTitle}' thiếu tiêu đề hoặc nội dung.";
+            }
+        }
+
+        return $errors;
+    }
+
 }
