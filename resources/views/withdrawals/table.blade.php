7045e7803e40d063b6ae743c68d88447e615e8a2
anh23256 <anhldph45102@fpt.edu.vn>
2025-02-27 14:42:41 +0700

    refactor-filter-withdraw

--- resources/views/withdrawals/table.blade.php
+++ resources/views/withdrawals/table.blade.php
@@ -4,10 +4,11 @@
             <thead class="table-light">
             <tr>
                 <th>STT</th>
+                <th>Ngân hàng</th>
                 <th>Tên chủ tài khoản</th>
                 <th>Số tài khoản</th>
-                <th>Ngân hàng</th>
                 <th>Số tiền</th>
+                <th>QR</th>
                 <th>Trạng thái</th>
                 <th>Ngày yêu cầu</th>
                 <th>Ngày xác nhận</th>
@@ -18,17 +19,29 @@
             @foreach ($withdrawals as $withdrawal)
                 <tr>
                     <td>{{ $loop->index + 1 }}</td>
-                    <td>{{ $withdrawal->account_holder ?? 'Không có thông tin' }}</td>
-                    <td><span class="text-danger">{{ $withdrawal->account_number ?? 'Không có thông tin' }}</span>
-                    </td>
                     <td>{{ \Illuminate\Support\Str::limit($withdrawal->bank_name ?? 'Không có thông tin',40) }}</td>
-                    <td>{{ number_format($withdrawal->amount) }} VND</td>
+                    <td>{{ $withdrawal->account_holder ?? 'Không có thông tin' }}</td>
+                    <td><span
+                            class="text-danger">{{ $withdrawal->account_number ?? 'Không có thông tin' }}</span>
+                    </td>
+                    <td>{{ number_format($withdrawal->amount ?? 0) }} VND</td>
                     <td>
-                        @if ($withdrawal->status === 'completed')
+                        <img
+                            id="thumbnail-{{ $withdrawal->id }}"
+                            class="img-thumbnail img-preview"
+                            width="50"
+                            height="50"
+                            src="{{ \Illuminate\Support\Facades\Storage::url($withdrawal->qr_code ?? '') }}"
+                            alt="QR Code {{ $withdrawal->id }}"
+                            style="cursor: pointer;"
+                        />
+                    </td>
+                    <td>
+                        @if ($withdrawal->status === 'Thành công')
                             <span class="badge bg-success w-100">
                                     Hoàn thành
                                 </span>
-                        @elseif($withdrawal->status === 'pending')
+                        @elseif($withdrawal->status === 'Đang xử lý')
                             <span class="badge bg-warning w-100">
                                     Đang xử lý
                                 </span>
@@ -43,7 +56,7 @@
                     <td>{!! $withdrawal->completed_date ? \Carbon\Carbon::parse($withdrawal->completed_date)->format('d/m/Y') : '<span class="btn btn-sm btn-soft-warning">Chưa xác nhận</span>' !!}
                     </td>
                     <td>
-                        <a href="{{ route('admin.approvals.courses.show', $withdrawal->id) }}">
+                        <a href="{{ route('admin.withdrawals.show', $withdrawal->id) }}">
                             <button class="btn btn-sm btn-info edit-item-btn">
                                 <span class="ri-eye-line"></span>
                             </button>
