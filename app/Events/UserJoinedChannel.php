b319a4430588894ea9af8818eb08f92dd6b25cbf
TungTvPh81102 <quaixe121811@gmail.com>
2025-03-07 16:00:49 +0700

    modify chat api

---
+++ app/Events/UserJoinedChannel.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserJoinedChannel implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+    public $conversation;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($user, $conversation)
+    {
+        $this->user = $user;
+        $this->conversation = $conversation;
+    }
+
+    public function broadcastOn()
+    {
+        return new PrivateChannel('conversation.' . $this->conversation->id);
+    }
+
+    public function broadcastAs()
+    {
+        return 'UserJoinedChannel';
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'user' => [
+                'id' => $this->user->id,
+                'name' => $this->user->name,
+                'avatar' => $this->user->avatar
+            ],
+            'conversation_id' => $this->conversation->id,
+            'joined_at' => now()->toDateTimeString()
+        ];
+    }
+}
